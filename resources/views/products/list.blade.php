4ad43d9f01f10d474d3a619d3fd34d9a7a91b58a
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-07 17:10:14 +0600

    Routing Completed

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -11,13 +11,19 @@
     <div class='min-h-screen max-w-screen-2xl'>
         <h1 class='bg-black text-white text-center text-3xl font-semibold py-3'>Simple Laravel CRUD</h1>
 
-        @if (Session::has('success'))
+        <div class="w-[1000px] mx-auto flex justify-end mt-4">
+            <div class="">
+                <a href="{{ route('products.create') }}" class="btn bg-blue-900 text-white p-2 rounded-md">create</a>
+            </div>
+        </div>
+
+        <!-- @if (Session::has('success'))
         <div class="pt-2 mt-4">
             <div class="">
                 {{ Session::get('success') }}
             </div>
         </div>
-        @endif
+        @endif -->
 
         <div class='bg-white shadow-2xl w-[1000px] mx-auto my-5 rounded-md'>
             <h1 class="text-center text-white text-2xl bg-black py-2 rounded-md">Products</h1>
